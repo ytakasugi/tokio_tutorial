c105552aa868305caa1cdbd723109fa788c5aae4
ytakasugi <sh7.tibi0129@gmail.com>
2021-08-14 03:57:20 +0900

    change files

--- my-redis/src/main.rs
+++ my-redis/src/main.rs
@@ -1,4 +1,6 @@
 use tokio::net::TcpListener;
+use std::collections::HashMap;
+use std::sync::{Arc, Mutex};
 
 mod process;
 use crate::process::process;
@@ -8,13 +10,21 @@ async fn main() {
     // リスナーをこのアドレスにバインドする
     let listener = TcpListener::bind("127.0.0.1:6379").await.unwrap();
 
+    println!("Listening");
+
+    let db = Arc::new(Mutex::new(HashMap::new()));
+
     loop {
         // タプルの2つ目の要素は、新しいコネクションのIPとポートの情報を含んでいる
         let (socket, _) = listener.accept().await.unwrap();
+
+        let db = db.clone();
+
+        println!("Accepted");
         // それぞれのインバウンドソケットに対して新しいタスクを spawn する
         // ソケットは新しいタスクに move され、そこで処理される
         tokio::spawn(async move {
-            process(socket).await;
+            process(socket, db).await;
         });
     }
 }
--- my-redis/src/process.rs
+++ my-redis/src/process.rs
@@ -1,16 +1,40 @@
+use std::collections::HashMap;
+use std::sync::{Arc, Mutex};
+
+use bytes::Bytes;
 use tokio::net::TcpStream;
 use mini_redis::{Connection, Frame};
+use mini_redis::Command::{self, Get, Set};
 
-pub async fn process(socket: TcpStream) {
+type Db = Arc<Mutex<HashMap<String, Bytes>>>;
+
+pub async fn process(socket: TcpStream, db: Db) {
     // `Connection` を使うことで、バイト列ではなく、Redis の
     // 「フレーム」を読み書きできるようになる
     // この `Connection` 型は mini-redis で定義されている
     let mut connection = Connection::new(socket);
 
-    if let Some(frame) = connection.read_frame().await.unwrap() {
-        println!("GOT: {:?}", frame);
-
-        let response = Frame::Error("unimplemented".to_string());
+    while let Some(frame) = connection.read_frame().await.unwrap() {
+        let response = match Command::from_frame(frame).unwrap() {
+            Set(cmd) => {
+                let mut db = db.lock().unwrap();
+                // `Vec<u8>`として保存する
+                db.insert(cmd.key().to_string(), cmd.value().clone());
+                Frame::Simple("OK".to_string())
+            }
+            Get(cmd) => {
+                let db = db.lock().unwrap();
+                if let Some(value) = db.get(cmd.key()) {
+                    // `Frame::Bulk` はデータが Bytes` 型であることを期待する
+                    // この型についてはのちほど解説する
+                    // とりあえずここでは `.into()` を使って `&Vec<u8>` から `Bytes` に変換する
+                    Frame::Bulk(value.clone().into())
+                } else {
+                    Frame::Null
+                }
+            }
+            cmd => panic!("unimplemented {:?}", cmd),
+        };
         connection.write_frame(&response).await.unwrap();
     }
 }
